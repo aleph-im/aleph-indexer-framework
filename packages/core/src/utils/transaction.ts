8578301faa54c740577950441588978923696afc
Mike Hukiewitz <mike.hukiewitz@robotter.ai>
2022-10-04 18:18:33 -0500

    add utils/transaction.ts docs

--- packages/core/src/utils/transaction.ts
+++ packages/core/src/utils/transaction.ts
@@ -6,6 +6,11 @@ import {
 } from '../types.js'
 import { isParsedIx } from './instruction.js'
 
+/**
+ * Returns true if at least one instruction in the transaction fulfills the check function.
+ * @param tx Transaction to check.
+ * @param check Function to check the instructions with.
+ */
 export function txIxsCheck(
   tx: AlephParsedTransaction | ParsedTransactionV1,
   check: (ix: AlephParsedInstruction) => boolean,
@@ -20,6 +25,11 @@ export function txIxsCheck(
   return false
 }
 
+/**
+ * Returns true if the transaction contains an instruction originating from a given program ID.
+ * @param tx Transaction to check.
+ * @param programId Program ID to check for.
+ */
 export function txHasProgramId(
   tx: AlephParsedTransaction | ParsedTransactionV1,
   programId: PublicKey | string,
@@ -30,6 +40,11 @@ export function txHasProgramId(
   )
 }
 
+/**
+ * Returns true if the transaction contains an instruction for a given parsed instruction type.
+ * @param tx Transaction to check.
+ * @param type Instruction type to check for.
+ */
 export function txHasType(
   tx: AlephParsedTransaction | ParsedTransactionV1,
   type: string,
@@ -40,6 +55,25 @@ export function txHasType(
   )
 }
 
+/**
+ * Returns the raw SPL token balance of an account in a transaction.
+ * @param tx Transaction to check.
+ * @param address Address of the SPL token account.
+ * @param post True if the balance should be the balance after the transaction, false if it should be the balance before the transaction.
+ *
+ * @note:
+ * Corner cases, which this function handles:
+ *
+ * 1. After some events like "closeAccount" the balance may not change
+ * and not be present on "postTokenBalances", so pick it from "preTokenBalances".
+ *
+ * 2. For token "So11111111111111111111111111111111111111112" (Wrapped SOL) if we close the account on the same tx,
+ * the postTokenBalance will be 0 and the postBalance will be the held amount on that account. Set it as 0 at the moment.
+ * An example: https://solscan.io/tx/5D6nddfkvRzFe9VHAmZ73YXeqsNQDRjw1qp9bfzhoEQUA6X4Yxy3SXL7rXw1UVsAhRPHnBkaBcMTGiPJouBBK1ZY
+ *
+ * @todo: calculate tx inner balances (taking preBalances and aggregating each tx)
+ *
+ */
 export function getTokenBalance(
   tx: AlephParsedTransaction | ParsedTransactionV1,
   address: string,
@@ -54,20 +88,6 @@ export function getTokenBalance(
   let balanceInfo = tx.meta?.[key]?.find(
     ({ accountIndex }) => accountIndex === balanceIndex,
   )
-
-  /**
-   * @note:
-   * Corner cases
-   *
-   * 1. After some events like "closeAccount" the balance may not change
-   * and not be present on "postTokenBalances", so pick it from "preTokenBalances"
-   *
-   * 2. For token "So11111111111111111111111111111111111111112" (Wrapped SOL) in which if we close the account on the same tx,
-   * the postTokenBalance will be 0 and the postBalance will be the holded amount on that account. Set it as 0 atm
-   * https://solscan.io/tx/5D6nddfkvRzFe9VHAmZ73YXeqsNQDRjw1qp9bfzhoEQUA6X4Yxy3SXL7rXw1UVsAhRPHnBkaBcMTGiPJouBBK1ZY
-   *
-   * @todo: calculate tx inner balances (taking preBalances and aggregating each tx)
-   */
 
   // 1.
   if (!balanceInfo && post) {
@@ -84,6 +104,12 @@ export function getTokenBalance(
   return balanceInfo?.uiTokenAmount.amount
 }
 
+/**
+ * Returns the raw SOL balance of an account in a transaction.
+ * @param tx Transaction to check.
+ * @param address Address of the account.
+ * @param post True if the balance should be the balance after the transaction, false if it should be the balance before the transaction.
+ */
 export function getBalance(
   tx: AlephParsedTransaction | ParsedTransactionV1,
   address: string,
@@ -97,7 +123,3 @@ export function getBalance(
   const amount = tx.meta?.[key]?.[balanceIndex]
   return amount ? String(amount) : undefined
 }
-
-// @todo: Refactor and remove this
-export const tx_has_type = txHasType
-export const tx_has_programId = txHasProgramId
