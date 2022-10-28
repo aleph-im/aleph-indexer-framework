7037285a37dea5cac629543f39e747b12ab311cf
Mike Hukiewitz <mike.hukiewitz@robotter.ai>
2022-10-28 17:07:20 +0200

    changes for testability

--- packages/marinade_finance/run.ts
+++ packages/marinade_finance/run.ts
@@ -20,7 +20,7 @@ async function main() {
   const projectId = 'marinade_finance'
   const dataPath = config.INDEXER_DATA_PATH || undefined // 'data'
   const transport =
-    (config.INDEXER_TRANSPORT as TransportType) || TransportType.LocalNet
+    (config.INDEXER_TRANSPORT as TransportType) || TransportType.Thread
 
   const transportConfig: any =
     tcpUrls || natsUrl ? { tcpUrls, natsUrl } : undefined
