5b772a7ed5dcd35032f9b6746b637d95c552f530
ricardocr987 <32191898+ricardocr987@users.noreply.github.com>
2022-11-01 14:59:05 +0100

    fix: @jest/globals error

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,13 +5,11 @@ module.exports = {
   resolver: 'ts-jest-resolver',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
-
+  verbose: true, // https://stackoverflow.com/questions/41093368/does-jest-swallow-console-log-statements-is-there-a-way-to-change-this
   globals: { 'ts-jest': { useESM: true } },
 
-  // from https://stackoverflow.com/a/57916712/15076557
-  // transformIgnorePatterns: [
-  //   'node_modules/(?!(module-that-needs-to-be-transformed)/)',
-  // ],
+  // from https://stackoverflow.com/a/57916712/15076557 & https://www.reddit.com/r/typescript/comments/tkvsgk/typescript_jest_unexpected_token_export/
+  transformIgnorePatterns: ['node_modules/@jest/globals'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
