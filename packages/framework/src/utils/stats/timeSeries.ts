3d4c52d2a45e5dd8a2d1ab5753a8ae19973da7d3
Mike Hukiewitz <mike.hukiewitz@robotter.ai>
2022-11-27 16:38:03 +0100

    Problem: Sometimes events would be included in the calculation of time frames, they didn't belong to.
    Solution: Use true dividing time frames when aggregating larger time frames, such that their start and end dates line up perfectly.

--- packages/framework/src/utils/stats/timeSeries.ts
+++ packages/framework/src/utils/stats/timeSeries.ts
@@ -1,5 +1,5 @@
 import {Utils} from '@aleph-indexer/core'
-import {DateTime, Interval} from 'luxon'
+import {DateTime, Duration, Interval} from 'luxon'
 import {
   clipDateRangesFromIterable,
   DateRange,
@@ -105,6 +105,22 @@ export class TimeSeriesStats<I, O> {
     for (const [timeFrameIndex, timeFrame] of sortedTimeFrames.entries()) {
       const timeFrameName = TimeFrame[timeFrame]
       console.log(`📈 processing ${type} ${timeFrameName} for ${account}`)
+
+      // @note: get the previous time frame, which is able to cleanly divide the current time frame.
+      let trueDivideTimeFrameIndex = timeFrameIndex
+      if (timeFrameIndex > 0) {
+        trueDivideTimeFrameIndex--
+      }
+      if (timeFrame !== TimeFrame.All && trueDivideTimeFrameIndex < 0) {
+        const timeFrameSize = Duration.fromObject({[timeFrameName.toLowerCase()]: 1}).toMillis()
+        while (true) {
+          if (trueDivideTimeFrameIndex <= 0) break
+          const smallerSize = Duration.fromObject({[TimeFrame[sortedTimeFrames[trueDivideTimeFrameIndex]].toLowerCase()]: 1}).toMillis()
+          if (timeFrameSize % smallerSize === 0) break
+          trueDivideTimeFrameIndex--
+        }
+      }
+
       const clipRangesStream = await this.stateDAL.getAllValuesFromTo(
         [account, type, timeFrame],
         [account, type, timeFrame],
@@ -205,7 +221,7 @@ export class TimeSeriesStats<I, O> {
           // const key = [account, type, timeFrame, startDate]
           const cache = {}
           const inputs =
-            timeFrameIndex === 0
+            trueDivideTimeFrameIndex === 0
               ? await getInputStream({
                   account,
                   startDate,
@@ -215,20 +231,20 @@ export class TimeSeriesStats<I, O> {
                   [
                     account,
                     type,
-                    sortedTimeFrames[timeFrameIndex - 1],
+                    sortedTimeFrames[trueDivideTimeFrameIndex],
                     startDate,
                   ],
                   [
                     account,
                     type,
-                    sortedTimeFrames[timeFrameIndex - 1],
+                    sortedTimeFrames[trueDivideTimeFrameIndex],
                     endDate,
                   ],
                 )
 
           let data: O | undefined
           for await (const value of inputs) {
-            const input = 'data' in value && timeFrameIndex !== 0 ? value.data : value
+            const input = 'data' in value && trueDivideTimeFrameIndex !== 0 ? value.data : value
             data = await aggregator({
               input,
               interval,
