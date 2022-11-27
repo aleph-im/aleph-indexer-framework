f99fb1ba2740a2a1030dc5bac079c3a57136086f
Mike Hukiewitz <mike.hukiewitz@robotter.ai>
2022-11-27 17:06:38 +0100

    add listGroupBy

--- packages/core/src/utils/common.ts
+++ packages/core/src/utils/common.ts
@@ -83,6 +83,20 @@ export function listSubstraction<T>(
   })
 }
 
+export function listGroupBy<T>(
+  a: T[],
+  getKey: (i: T) => string | number | symbol,
+): Record<string | number | symbol, T[]> {
+  return a.reduce((acc, curr) => {
+    const k = getKey(curr)
+    if (!acc[k]) {
+      acc[k] = []
+    }
+    acc[k].push(curr)
+    return acc
+  }, {} as Record<string | number | symbol, T[]>)
+}
+
 export function sortTimeStatsMap<T extends { interval: string }>(
   map: Record<string, T>,
   reverse = false,
