be7c993ba8e573a1b7612db3a5ab272340d6c21a
Mike Hukiewitz <mike.hukiewitz@robotter.ai>
2022-12-19 14:53:05 +0100

    add anchorInstructionParser test for Pyth

---
+++ packages/pyth/src/layouts/__tests__/anchorInstructionParser.spec.ts
@@ -0,0 +1,24 @@
+import {AnchorInstructionParser} from "@aleph-indexer/framework/dist/src/services/parser/src/anchorInstructionParser";
+import updPriceTxn from '../__mocks__/updPrice.json'
+import {PythEventType} from "../../types";
+import layout from "../../layouts/layout.js";
+import { PYTH_PROGRAM_ID } from "../../constants.js";
+import {AlephParsedInstruction} from "@aleph-indexer/core/dist";
+
+// prepare the anchor instruction parser
+const parser = new AnchorInstructionParser<PythEventType>(
+  PYTH_PROGRAM_ID,
+  layout[PYTH_PROGRAM_ID].name,
+  layout[PYTH_PROGRAM_ID].getInstructionType,
+  layout[PYTH_PROGRAM_ID].accountLayoutMap,
+  layout[PYTH_PROGRAM_ID].dataLayoutMap,
+)
+
+describe('Pyth: AnchorInstructionParser', () => {
+  it('should parse the instruction', () => {
+    const parsed = parser.parse(updPriceTxn) as AlephParsedInstruction
+    expect(parsed).toBeDefined()
+    expect(parsed.innerInstructions).toBeDefined()
+    expect(parsed.programId).toEqual(PYTH_PROGRAM_ID)
+  })
+})
