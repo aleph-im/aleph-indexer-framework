2a5de5b2825870f554dad72f16ee67f4fe0b73fe
amalcaraz <amalcaraz89@gmail.com>
2023-01-26 19:37:01 +0100

    updated indexing example

--- packages/indexer-example/src/domain/main.ts
+++ packages/indexer-example/src/domain/main.ts
@@ -17,23 +17,23 @@ export default class MainDomain
   }
 
   async discoverAccounts(): Promise<AccountIndexerConfigWithMeta<undefined>[]> {
-    // const alephTokenEth = '0x27702a26126e0b3702af63ee09ac4d1a084ef628'
+    const alephTokenEth = '0x27702a26126e0b3702af63ee09ac4d1a084ef628'
     const alephTokenSol = '3UCMiSnkcnkPE1pgQ5ggPCBv6dXgVUy16TmMUe1WpG9x'
     const alephMessages = '0x166fd4299364B21c7567e163d85D78d2fb2f8Ad5'
 
     return [
-      // {
-      //   blockchainId: Blockchain.Ethereum,
-      //   account: alephTokenEth,
-      //   meta: undefined,
-      //   index: {
-      //     transactions: {
-      //       chunkDelay: 0,
-      //       chunkTimeframe: 1000 * 60 * 60 * 24,
-      //     },
-      //     state: false,
-      //   },
-      // },
+      {
+        blockchainId: Blockchain.Ethereum,
+        account: alephTokenEth,
+        meta: undefined,
+        index: {
+          transactions: {
+            chunkDelay: 0,
+            chunkTimeframe: 1000 * 60 * 60 * 24,
+          },
+          state: false,
+        },
+      },
       {
         blockchainId: Blockchain.Solana,
         account: alephTokenSol,
@@ -46,18 +46,18 @@ export default class MainDomain
           state: false,
         },
       },
-      // {
-      //   blockchainId: Blockchain.Ethereum,
-      //   account: alephMessages,
-      //   meta: undefined,
-      //   index: {
-      //     transactions: {
-      //       chunkDelay: 0,
-      //       chunkTimeframe: 1000 * 60 * 60 * 24,
-      //     },
-      //     state: false,
-      //   },
-      // },
+      {
+        blockchainId: Blockchain.Ethereum,
+        account: alephMessages,
+        meta: undefined,
+        index: {
+          transactions: {
+            chunkDelay: 0,
+            chunkTimeframe: 1000 * 60 * 60 * 24,
+          },
+          state: false,
+        },
+      },
     ]
   }
 }
