a25b7f9bf27404a1396460d9e6bd735fda1c6dd0
amalcaraz <amalcaraz89@gmail.com>
2023-02-11 00:51:34 +0100

    fix account case sensitiveness on ethereum indexer & fetcher clients

--- packages/ethereum/src/services/fetcher/client.ts
+++ packages/ethereum/src/services/fetcher/client.ts
@@ -1,11 +1,15 @@
 import { ServiceBroker } from 'moleculer'
 import {
+  AccountEntityHistoryState,
+  AddAccountEntityRequestArgs,
   BaseFetcherClient,
   Blockchain,
   BlockchainRequestArgs,
+  DelAccountEntityRequestArgs,
   FetchAccountEntitiesByDateRequestArgs,
   FetchEntitiesByIdRequestArgs,
   FetcherClientI,
+  GetAccountEntityStateRequestArgs,
   IndexableEntityType,
 } from '@aleph-indexer/framework'
 
@@ -13,6 +17,37 @@ export class EthereumFetcherClient
   extends BaseFetcherClient
   implements FetcherClientI
 {
+  addAccountEntityFetcher(
+    args: Omit<AddAccountEntityRequestArgs, keyof BlockchainRequestArgs>,
+  ): Promise<void> {
+    args.account = args.account.toLowerCase()
+    return super.addAccountEntityFetcher(args)
+  }
+
+  delAccountEntityFetcher(
+    args: Omit<DelAccountEntityRequestArgs, keyof BlockchainRequestArgs>,
+  ): Promise<void> {
+    args.account = args.account.toLowerCase()
+    return super.delAccountEntityFetcher(args)
+  }
+
+  getAccountEntityFetcherState(
+    args: Omit<GetAccountEntityStateRequestArgs, keyof BlockchainRequestArgs>,
+  ): Promise<AccountEntityHistoryState<unknown> | undefined> {
+    args.account = args.account.toLowerCase()
+    return super.getAccountEntityFetcherState(args)
+  }
+
+  fetchAccountEntitiesByDate(
+    args: Omit<
+      FetchAccountEntitiesByDateRequestArgs,
+      keyof BlockchainRequestArgs
+    >,
+  ): Promise<void | AsyncIterable<string[]>> {
+    args.account = args.account.toLowerCase()
+    return super.fetchAccountEntitiesByDate(args)
+  }
+
   // @note: Right now we are caching all logs on the same instance than the accountLogHistory is saving the indexes
   // @todo: Implement getLogsByIds method on ethereum client, and do partitions by log id instead account
   async fetchEntitiesById(
@@ -20,7 +55,7 @@ export class EthereumFetcherClient
   ): Promise<void> {
     if (args.type === IndexableEntityType.Log && args.meta) {
       const { account } = args.meta as FetchAccountEntitiesByDateRequestArgs
-      ;(args as any).partitionKey = account
+      ;(args as any).partitionKey = account.toLowerCase()
     }
 
     return super.fetchEntitiesById(args)
--- packages/ethereum/src/services/indexer/client.ts
+++ packages/ethereum/src/services/indexer/client.ts
@@ -1,21 +1,64 @@
 import { ServiceBroker } from 'moleculer'
 import {
+  AccountIndexerRequestArgs,
+  AccountIndexerState,
   BaseIndexerClient,
   Blockchain,
   BlockchainRequestArgs,
+  EntityRequest,
+  GetAccountIndexingEntityStateRequestArgs,
+  GetEntityPendingRequestsRequestArgs,
   IndexerClientI,
   InvokeMethodRequestArgs,
 } from '@aleph-indexer/framework'
 
+// @note: partitionKey is based on account param, and the "hash(account)" could change depending
+// on the address format and case-sensitiveness
 export class EthereumIndexerClient
   extends BaseIndexerClient
   implements IndexerClientI
 {
+  indexAccount(
+    args: Omit<AccountIndexerRequestArgs, keyof BlockchainRequestArgs>,
+    broadcast = false,
+  ): Promise<void> {
+    args.account = args.account.toLowerCase()
+    return this.indexAccount(args, broadcast)
+  }
+
+  deleteAccount(
+    args: Omit<AccountIndexerRequestArgs, keyof BlockchainRequestArgs>,
+    broadcast = false,
+  ): Promise<void> {
+    args.account = args.account.toLowerCase()
+    return super.deleteAccount(args, broadcast)
+  }
+
+  getAccountState(
+    args: Omit<
+      GetAccountIndexingEntityStateRequestArgs,
+      keyof BlockchainRequestArgs
+    >,
+  ): Promise<AccountIndexerState | undefined> {
+    args.account = args.account.toLowerCase()
+    return super.getAccountState(args)
+  }
+
   invokeDomainMethod(
     args: Omit<InvokeMethodRequestArgs, keyof BlockchainRequestArgs>,
   ): Promise<unknown> {
     args.account = args.account.toLowerCase()
     return super.invokeDomainMethod(args)
+  }
+
+  getEntityPendingRequests(
+    args: Omit<
+      GetEntityPendingRequestsRequestArgs,
+      keyof BlockchainRequestArgs
+    >,
+  ): Promise<EntityRequest[]> {
+    args.account = args.account ? args.account.toLowerCase() : args.account
+    return super.getEntityPendingRequests(args)
   }
 }
 
